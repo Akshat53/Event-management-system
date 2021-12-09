e83e38ade0d3d1e330d2fdc546ccb04ca1dfeac2
Akshat Singh <singh.akshat.akshat26@gmail.com>
2021-12-09 17:46:27 +0530

    fix

--- dbs/connect.php
+++ dbs/connect.php
@@ -6,6 +6,9 @@
 // $user="root";
 // $password="";
 // $dbname="abhiruchi";
+
+// $connect=mysqli_connect($host,$user,$password,$dbname);
+
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
@@ -15,5 +18,5 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
