3caf7b7a35e02b8f2951ec9cbb968f89c7541fe2
Akshat Singh <singh.akshat.akshat26@gmail.com>
2021-12-09 17:37:25 +0530

    deploy

--- dbs/connect.php
+++ dbs/connect.php
@@ -1,4 +1,5 @@
-<?php
+
+<!-----------<?php
 
     $host="localhost";
     $user="root";
@@ -8,4 +9,17 @@
     $connect=mysqli_connect($host,$user,$password,$dbname);
 
 
+?>-------------->
+
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
